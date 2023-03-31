5216de18387ffed1b6ed00c96e7ae54716c2c827
Mikolaj Mindykowski <mikolaj.mindykowski@gmail.com>
2023-03-31 16:41:22 +0200

    dlugosc stringa

---
+++ main.js
@@ -0,0 +1,19 @@
+let text1 = 'Uwielbiam JavaScript';
+text1.length;
+
+console.log(text1.length)
+
+let text2 = 'Jestem świetnym programistą';
+text2.lenght;
+
+console.log(text2.length)
+
+function dłuższy (x,y) {
+    if (length <= length) {
+        return y;
+    }   else {
+        return x;
+    }
+}
+
+console.log(dłuższy(text1, text2));
